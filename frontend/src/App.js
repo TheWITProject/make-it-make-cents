3c2db0b4ed0b4a0efa0687cdd701d6a3494bedf0
rachelnatalie <rachelguilbaud7@gmail.com>
2022-03-23 18:16:05 -0400

    First attempt at creating login section for welcome page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,9 @@ function App() {
   return (
     <div className="welcome">
       <div className="logo">Location for logo</div>
-      Lalalal stuff in the middle for you guys to do :)
+      <div className="login">Username:</div>
+      <div className="login">Password:</div>
+      <div className="signinbutton"><button> Sign in </button></div>
       <div className="links">Location for links</div>
     </div>
   );
