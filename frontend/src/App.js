5f8000a20c838f7b5adfa0520d15dec0209d7a57
seh5 <seh5@njit.edu>
2022-03-23 16:21:48 -0400

    First attempt at New User Registration

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,29 @@
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="welcome">
-      <div className="logo">Location for logo</div>
-      Lalalal stuff in the middle for you guys to do :)
-      <div className="links">Location for links</div>
+    <div className="registerNewUser">
+        <h3>
+          New User? Register Below!
+        </h3>
+        <p>
+          Username:
+        </p>
+        <p>
+          e-mail:
+        </p>
+        <p>
+          Create Password:
+        </p>
+        <p>
+          Confirm Password:
+        </p>
+        <a
+          className="completionButton"
+        >
+         <strong>REGISTER</strong>
+        </a>
     </div>
   );
 }
