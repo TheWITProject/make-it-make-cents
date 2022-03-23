52b13c0062abcb276cd34551f55eb59492a5dffc
Stacy Kim <stacy@awning.com>
2022-03-22 17:40:09 -0700

    Delete some stuff

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,6 @@ function App() {
   return (
     <div className="welcome">
       <div className="logo">Location for logo</div>
-      Lalalal stuff in the middle for you guys to do :)
       <div className="links">Location for links</div>
     </div>
   );
