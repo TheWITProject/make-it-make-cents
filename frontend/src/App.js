77c0ba2a407a3fbd630bc9fd69cd53b0495bd285
Stacy Kim <stacy@awning.com>
2022-03-24 13:02:17 -0700

    Remove unused logo import

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -6,33 +5,22 @@ function App() {
     <div className="welcome">
       <div className="logo">Location for logo</div>
       <div className="about-us">
-        <p>MAKE IT MAKE CENTS</p> 
+        <p>MAKE IT MAKE CENTS</p>
         <p>ABOUT US</p>
       </div>
       <div className="login">Username:</div>
       <div className="login">Password:</div>
-      <div className="signinbutton"><button> Sign in </button></div>
+      <div className="signinbutton">
+        <button> Sign in </button>
+      </div>
       <div className="registerNewUser">
-      <h3>
-          New User? Register Below!
-        </h3>
-        <div className="username">
-          Username:
-        </div>
-        <div className="email">
-          e-mail:
-        </div>
-        <div className="password">
-          Create Password:
-        </div>
-        <div className="password">
-          Confirm Password:
-        </div>
-        <a
-          className="completionButton"
-          href="/"
-        >
-         <strong>REGISTER</strong>
+        <h3>New User? Register Below!</h3>
+        <div className="username">Username:</div>
+        <div className="email">e-mail:</div>
+        <div className="password">Create Password:</div>
+        <div className="password">Confirm Password:</div>
+        <a className="completionButton" href="/">
+          <strong>REGISTER</strong>
         </a>
       </div>
       <div className="links">Location for links</div>
