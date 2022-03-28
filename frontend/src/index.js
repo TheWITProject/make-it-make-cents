7bfd85bf28cd70be9aa5b4cee521fb1b0e83cc4f
Stacy Kim <stacy@awning.com>
2022-03-28 15:29:12 -0700

    Correctly render the BrowserRouter in the DOM

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,35 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { render } from "react-dom";
-import { 
-  BrowserRouter,
-  Routes,
-  Route,
-  Link,
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import FinAssess from "./routes/financial-assessment";
-import About from "./routes/about-us";
-import Budget from "./routes/budget-calculator";
-
-const rootElement = document.getElementById("root");
-render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="financial-assessment" element={<FinAssess />} />
-      <Route path="about-us" element={<About />} />
-      <Route path="budget-calculator" element={<Budget />} />
-    </Routes>
-  </BrowserRouter>,
-  rootElement
-);
+import FinAssess from './routes/financial-assessment';
+import About from './routes/about-us';
+import Budget from './routes/budget-calculator';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="financial-assessment" element={<FinAssess />} />
+        <Route path="about-us" element={<About />} />
+        <Route path="budget-calculator" element={<Budget />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
