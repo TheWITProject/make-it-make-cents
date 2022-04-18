0c7c694d2e4e8ed943ea28cfa9c8d8c7ac56e041
elypalacios <elizabethpalacios.cs@gmail.com>
2022-04-18 10:04:45 -0400

    I have created the question and answer database table for all 11 questions. I also have created the Persona table and the Assessment Results Table that will store the different options a user will get when taking the assesment. I do have trouble with the scoring rank

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -2,8 +2,11 @@ from django.contrib import admin
 from .models import user
 from .models import Question
 from .models import Choice
+from .models import Persona
+from .models import AssesmentResult
 
 admin.site.register(user)
 admin.site.register(Question)
 admin.site.register(Choice)
-
+admin.site.register(Persona)
+admin.site.register(AssesmentResult)
