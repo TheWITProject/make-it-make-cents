2c0c8d9744ef9d4a30a6ff286b35821732acd64c
elypalacios <elizabethpalacios.cs@gmail.com>
2022-04-18 17:59:14 -0400

    Added links to the financial assessment and budget calculator

--- frontend/src/routes/Home.js
+++ frontend/src/routes/Home.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import '../App.css';
+import { Link } from 'react-router-dom';
 
 export default function Home() {
     return (
       <div> 
       <div className="Logout">
         <h1> LOGOUT </h1>
-        <img src = "./Images/HorizontalMMCLogo.jpg" alt ="MMC" className="Left" /> 
+        <img src = "./Images/HorizontalMMCLogo.jpg" alt ="MMC" className="Left"/> 
       <div className="WelcomeUser">
         <h2>Hello, (User's Name) ! What would you like to do today? </h2>
-      <div className="Button1"> <h1 > Test Your Financial Knowledge  </h1><button> Click here </button> </div>
-      <div className="Button2"> <h1 >  Create A Budget </h1><button> Click here </button> </div>
+      <div className="Button1"> <h1 ><nav><Link to="/financial-assessment">Test Your Financial Knowledge</Link></nav>  </h1></div>
+      <div className="Button2"> <h1 > <nav>
+          <Link to="/budget-calculator"> Create A Budget </Link>
+        </nav></h1> </div>
       </div>
       </div>
       </div>
