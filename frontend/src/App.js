7559682b2d89c10dbc58387f8848646d17da101d
rachelnatalie <rachelguilbaud7@gmail.com>
2022-05-17 00:50:02 -0400

    Added divider and fixed buttons, next is to add input fields and image and text to left

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ console.log(logo);
 
 function App() {
   return (
+    <div className="container">
     <div className="welcome">
       <div className="logo">
       <img src={logo} alt="Logo"/>
@@ -17,13 +18,15 @@ function App() {
       <div className="username"><p>Username</p></div>
       <div className="password"><p>Password</p></div>
       <div className="signin"><p>SIGN IN</p></div>
+      <div className="line"><hr></hr></div>
       <div className="newusers"><p>New Users</p></div>
+      <div className="name"><p>First Name</p></div>
       <div className="username"><p>Username</p></div>
       <div className="email"><p>Email Address</p></div>
       <div className="password"><p>Create Password</p></div>
       <div className="password"><p>Confirm Password</p></div>
-      <div className="name"><p>First Name</p></div>
       <div className="register"><p>REGISTER</p></div>
+    </div>
     </div>
   );
 }
