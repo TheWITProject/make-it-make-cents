6f2640ba27504ef4dd076282fb523345c49faae0
rachelnatalie <rachelguilbaud7@gmail.com>
2022-05-20 17:05:34 -0400

    finalized loginpage and added link to home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,64 @@
 import React from 'react';
 import './App.css';
-import {Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import logo from './horizontallogo.png';
 
-
-console.log(logo); 
+console.log(logo);
 
 function App() {
   return (
     <div className="container">
-    <div className="welcome">
-      <div className="logo">
-      <img src={logo} alt="Logo"/>
+      <div className="welcome">
+        <div className="logo">
+          <img src={logo} alt="Logo" />
+          <div className="welcome">
+            <p>Welcome</p>
+          </div>
+        </div>
+        <div className="returningusers">
+          <p>Returning Users</p>
+        </div>
+        <div className="username">
+          <p>Username</p>
+        </div>
+        <div className="password">
+          <p>Password</p>
+        </div>
+        <nav>
+          <Link to="/Home">
+            <div className="signin">
+              <p>SIGN IN</p>
+            </div>
+          </Link>
+        </nav>
+        <div>
+          <img src={require('./welcomepage.png')} className="position" />
+        </div>
+        <div className="line">
+          <hr></hr>
+        </div>
+        <div className="newusers">
+          <p>New Users</p>
+        </div>
+        <div className="name">
+          <p>First Name</p>
+        </div>
+        <div className="username">
+          <p>Username</p>
+        </div>
+        <div className="email">
+          <p>Email Address</p>
+        </div>
+        <div className="password">
+          <p>Create Password</p>
+        </div>
+        <div className="password">
+          <p>Confirm Password</p>
+        </div>
+        <div className="register">
+          <p>REGISTER</p>
+        </div>
       </div>
-      <div className="welcome"><p>Welcome</p></div>
-      <div className="returningusers"><p>Returning Users</p></div>
-      <div className="username"><p>Username</p></div>
-      <div className="password"><p>Password</p></div>
-      <div className="signin"><p>SIGN IN</p></div>
-      <div className="line"><hr></hr></div>
-      <div className="newusers"><p>New Users</p></div>
-      <div className="name"><p>First Name</p></div>
-      <div className="username"><p>Username</p></div>
-      <div className="email"><p>Email Address</p></div>
-      <div className="password"><p>Create Password</p></div>
-      <div className="password"><p>Confirm Password</p></div>
-      <div className="register"><p>REGISTER</p></div>
-    </div>
     </div>
   );
 }
